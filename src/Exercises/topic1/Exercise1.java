682605393b55e6d13c861cfa3ad052e858830d74
lucasaarj04 <lucas.aarj04@gmail.com>
2025-11-30 18:52:56 -0300

    Add files via upload

---
+++ src/Exercises/topic1/Exercise1.java
@@ -0,0 +1,7 @@
+package src.Exercises.topic1;
+
+public class Exercise1 {
+    public static void run() {
+        System.out.println("Executando exercício 1");
+    }
+}
---
+++ src/app/Main.java
@@ -0,0 +1,9 @@
+package src.app;
+
+import src.Exercises.topic1.Exercise1;
+
+public class Main {
+    public static void main(String[] args) {
+        Exercise1.run();
+    }
+}
